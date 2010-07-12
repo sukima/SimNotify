45e847c85353c69de6d4ecc43fb247962541197f
Devin Weaver <suki@tritarget.org>
2010-07-11 20:46:21 -0400

    Cleans up scenario and manikin displays
    Cleans up several navigation links

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,4 +1,13 @@
 class Scenario < ActiveRecord::Base
   belongs_to :event
   belongs_to :manikin
+
+  def flags_as_strings
+    flags = Array.new
+    flags << "Staff Support" if staff_support
+    flags << "Moulage" if moulage
+    flags << "Video Debriefing" if video
+    flags << "Mobile Cart" if mobile
+    return flags
+  end
 end
