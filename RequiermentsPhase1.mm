7d01028afaf402bc95e674999b56185d0ca629a7
Devin Weaver <suki@tritarget.org>
2010-07-16 10:14:25 -0400

    Adds initial notes and diagrams of project

---
+++ RequiermentsPhase1.mm
@@ -0,0 +1,14 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1276010396905" ID="Freemind_Link_1544085023" MODIFIED="1276011193992" TEXT="Requierments Phase 1">
+<node CREATED="1276010850344" ID="_" MODIFIED="1276010862061" POSITION="right" TEXT="User must log in">
+<node CREATED="1276010862465" ID="Freemind_Link_195400443" MODIFIED="1276010872108" TEXT="Login must be easy to use"/>
+<node CREATED="1276010872305" ID="Freemind_Link_99313734" MODIFIED="1276010883659" TEXT="Use a text box with suggestions"/>
+</node>
+<node CREATED="1276010898367" ID="Freemind_Link_957189076" MODIFIED="1276010936312" POSITION="right" TEXT="Offer a flat form to schedule class"/>
+<node CREATED="1276010913566" ID="Freemind_Link_1059514057" MODIFIED="1276010943610" POSITION="right" TEXT="Offer a wizard to populate flat form"/>
+<node CREATED="1276010947819" ID="Freemind_Link_215134794" MODIFIED="1276010956007" POSITION="right" TEXT="Information will be stored in a database"/>
+<node CREATED="1276010956267" ID="Freemind_Link_979021835" MODIFIED="1276010979350" POSITION="right" TEXT="Information will be sent via email to team"/>
+<node CREATED="1276010979690" ID="Freemind_Link_1253109888" MODIFIED="1276011003188" POSITION="right" TEXT="Once aproved email notification will be sent to original requestor"/>
+</node>
+</map>
