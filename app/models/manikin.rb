51fbd317680b71c21ed60374322b634f56ab2cb7
Devin Weaver <suki@tritarget.org>
2010-07-23 15:49:15 -0400

    Fixes human readble fields for manikin
    Fixes manikin validation.
    Adds localization for human readable attributes.

--- app/models/manikin.rb
+++ app/models/manikin.rb
@@ -1,6 +1,10 @@
 class Manikin < ActiveRecord::Base
   belongs_to :manikin_req_type
 
+  validates_presence_of :manikin_req_type_id, :message => "must be assigned"
+  validates_presence_of :name, :sim_type
+  validates_uniqueness_of :name
+
   def name_and_type
     "#{name} (#{sim_type})"
   end
