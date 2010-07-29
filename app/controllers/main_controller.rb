ebc7089f1c31154a76566dcb5869c58205f7f71a
Devin Weaver <suki@tritarget.org>
2010-07-29 10:03:42 -0400

    Fixes logout bug
    Crashed when there was no current_instructor

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,7 @@ class MainController < ApplicationController
   def index
     unless logged_in?
       redirect_to login_url
+      return
     end
     @events_in_queue = @current_instructor.events.find(:all, :conditions => { :submitted => false, :approved =>  false })
     @events_submitted = @current_instructor.events.find(:all, :conditions => { :submitted => true, :approved =>  false })
