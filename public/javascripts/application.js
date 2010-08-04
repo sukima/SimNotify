a649b590c74095107210de3b1cab16833ac70879
Devin Weaver <suki@tritarget.org>
2010-08-04 09:20:39 -0400

    jQuery initial setup
    Custom submitWithAjax function.
    Sets up a js format for AJAX interaction.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Configure jQuery {{{1
+jQuery.ajaxSetup({
+    'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+});
+
+// jQuery Custom Functions {{{1
+jQuery.fn.submitWithAjax = function() {
+    this.submit(function() {
+        $.post(this.action, $(this).serialize(), null, "script");
+        return false;
+    });
+    return this;
+};
+
+// Document Ready {{{1
+$(document).ready(function() {
+    
+});
+
+// vim:set fdm=marker:
