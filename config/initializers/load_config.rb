4ac9c83094769d727db238ddf74cd58d7e17dc25
Devin Weaver <suki@tritarget.org>
2010-08-06 09:59:34 -0400

    Better application configuration layout
    This will allow for global parameters to be assigned regardless of
    environment. It takes the global config variables and adds them to the
    ones found under the environment config variables.
    
        var1: "foo"
        environment:
          development:
            var2: "bar"
    
    Will produce the following code if in the development environment:
    
        APP_CONFIG['var1']    #=> "foo"
        APP_CONFIG['var2']    #=> "bar"

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -2,4 +2,6 @@
 # See RailCast:
 # http://railscasts.com/episodes/85-yaml-configuration-file
 
-APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")[RAILS_ENV]
+APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
+APP_CONFIG.merge!(APP_CONFIG['environment'][RAILS_ENV])
+APP_CONFIG.delete('environment')
