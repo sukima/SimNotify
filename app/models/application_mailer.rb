075610f375cbe1abaec445c5158bc7c46af4acb7
Devin Weaver <suki@tritarget.org>
2010-08-06 09:55:01 -0400

    Adds views to mailer
    Since ActionMailer requires two views for multipart emails this adds a
    rake task that will read a partial view writen in markdown and build the
    needed text and html views. The html view will render the partial into a
    variable then translate it through Maruku. Were as the text view only
    renders the partial. This allows erb code to stay in the partial view
    and prevents Maruku choking on them. Translating to markdown is done on
    the fly. This includes the maruki gem in environment.rb and the
    generated files in .gitignore

--- app/models/application_mailer.rb
+++ app/models/application_mailer.rb
@@ -1,10 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-
   def welcome_email(instructor)
     recipients  instructor.email
     from        APP_CONFIG['system_email_address']
     subject     "Welcome to SimNotify;. Thank you for registering."
     sent_on     Time.now
-    body        {:instructor => instructor}
+    body        {:instructor => instructor, :name => instructor.name_hash}
   end
 end
