7513cfb1026667d6b9b6c7b75e611ce9d1d8daff
Devin Weaver <suki@tritarget.org>
2010-08-08 21:13:41 -0400

    Adds calendar controller with fullCalendar plugin

---
+++ app/controllers/calendar_controller.rb
@@ -0,0 +1,7 @@
+class CalendarController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,8 @@ ActionController::Routing::Routes.draw do |map|
   map.autoconnect_map "main/autocomplete_map", :controller => "main", :action => "autocomplete_map"
   map.connect "main/autocomplete_map.:format", :controller => "main", :action => "autocomplete_map"
 
+  map.connect 'calendar/:action', :controller => 'calendar'
+
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
