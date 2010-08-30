19bc4ea0f708f913fce40cadf413647a31736d42
Devin Weaver <suki@tritarget.org>
2010-08-30 14:25:58 -0400

    Moves need_flags_as_words to model for more access
    This is only a string function not really a view function. While in the
    view helper for events it is not accessable by all other views that will
    need this method. Moved to the event model to facilitate better access.

--- app/models/event.rb
+++ app/models/event.rb
@@ -98,4 +98,14 @@ class Event < ActiveRecord::Base
     end
     return false
   end
+
+  def need_flags_as_words
+    needs = []
+    needs_hash = collective_need_flags
+    needs << "staff support" if needs_hash[:staff_support]
+    needs << "moulage" if needs_hash[:moulage]
+    needs << "video" if needs_hash[:video]
+    needs << "mobile" if needs_hash[:mobile]
+    return needs.to_sentence
+  end
 end
