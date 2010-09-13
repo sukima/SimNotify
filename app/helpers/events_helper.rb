750139c2da063421a4cbe6fb497f267a66a9008f
Devin Weaver <suki@tritarget.org>
2010-09-13 11:19:28 -0400

    Adds session_description helper
    For submit and revoke views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def session_description(event)
+    ret = h event.start_time
+    ret += " (#{distance_of_time_in_words(event.start_time, event.end_time)})"
+    ret += " - #{pluralize(event.scenarios.count, "senario")}"
+    ret += h event.need_flags_as_words if event.collective_has_needs
+    return ret
+  end
 end
