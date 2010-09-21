30482e0653b6cd914b3af0b5db5919acd0532b6b
Devin Weaver <suki@tritarget.org>
2010-09-21 11:16:13 -0400

    Adds special events to calenar

--- app/models/special_event.rb
+++ app/models/special_event.rb
@@ -3,5 +3,10 @@ class SpecialEvent < ActiveRecord::Base
 
   belongs_to :instructor
 
+  validates_presence_of :title
   validate :no_reverse_time_travel
+
+  def status_as_class
+    "special-event"
+  end
 end
