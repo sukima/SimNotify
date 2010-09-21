f9bd20dfe4da46e55ea252f1dff407cb9519fcaa
Devin Weaver <suki@tritarget.org>
2010-09-21 13:20:39 -0400

    Refactors table_sorter
    Adds a table_sorter helper method. Will incorporate all the javascript
    and HTML needed to sort tables with the jQuery tablesorter plugin.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,24 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def table_sorter(exclude_headers = [])
+    javascript "jquery.tablesorter"
+    stylesheet "tablesorter/style"
+    ret = ""
+    ret += "\n<script type=\"text/javascript\">\n"
+    ret += "  $(document).ready(function () {\n"
+    ret += "    $(\"#table_to_sort\").addClass(\"tablesorter\").tablesorter({\n"
+    #ret += "      debug: true,\n"
+    if !exclude_headers.empty?
+      header_str = ""
+      exclude_headers.each_with_index do |num, i|
+        header_str += (i > 0) ? ", " : ""
+        header_str += "#{num}: { sorter: false }"
+      end
+      ret += "      headers: { #{header_str} }\n"
+    end
+    ret += "    });\n"
+    ret += "  });\n"
+    ret += "</script>\n"
+    return ret
+  end
 end
