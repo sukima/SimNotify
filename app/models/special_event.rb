5fad1971b608aabba1d1f78c6078bfd10c0c487e
Devin Weaver <suki@tritarget.org>
2010-09-28 11:29:19 -0400

    Fixes special_event bugs
    Found through testing

--- app/models/special_event.rb
+++ app/models/special_event.rb
@@ -3,7 +3,7 @@ class SpecialEvent < ActiveRecord::Base
 
   belongs_to :instructor
 
-  validates_presence_of :title
+  validates_presence_of :title, :start_time, :end_time
   validate :no_reverse_time_travel
 
   def status_as_class
