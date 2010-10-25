7d7a0b143dc3034607b00e9f28d19f1c2df063f4
Devin Weaver <suki@tritarget.org>
2010-10-25 13:32:09 -0400

    Backup WIP

--- test/functional/calendar_controller_test.rb
+++ test/functional/calendar_controller_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
 class CalendarControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  setup :activate_authlogic
+
+  should route(:get, "/calendar").to(:action => :index)
+  should route(:get, "/calendar/events").to(:action => :events)
+
+  should require_logged_in
+  should require_logged_in.for(:events)
+
+#  logged_in_as(:instructor) do
+#    setup { @event = Factory(:special_event) }
+#    context "get index" do
+#      setup { get :index }
+#      should render_template :index
+#    end
+
+#    context "get events without params" do
+#      setup { get :events }
+#      should respond_with :not_acceptable
+#    end
+
+#    context "get events any format" do
+#      setup { get :events, { :start => 0, :end => 1 } }
+#      should respond_with :not_acceptable
+#    end
+
+#    context "get events json format" do
+#      setup { get :events, { :format => :json, :start => 0, :end => 1 } }
+#      should respond_with :success
+#    end
+#  end
 end
