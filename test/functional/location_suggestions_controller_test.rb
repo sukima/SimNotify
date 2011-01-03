a541a8a958dc4981b7740f829d30c09c396426e7
Devin Weaver <suki@tritarget.org>
2011-01-03 11:46:38 -0500

    Refactors location_suggestions_controller_test
    Moves to shoulda testing

--- test/functional/location_suggestions_controller_test.rb
+++ test/functional/location_suggestions_controller_test.rb
@@ -1,54 +1,71 @@
 require 'test_helper'
 
 class LocationSuggestionsControllerTest < ActionController::TestCase
-  def test_index
-    get :index
-    assert_template 'index'
-  end
+  setup :activate_authlogic
 
-  def test_show
-    get :show, :id => LocationSuggestion.first
-    assert_template 'show'
-  end
+  should_map_resources :location_suggestions
 
-  def test_new
-    get :new
-    assert_template 'new'
+  context "get :index" do
+    should_require_logged_in :action => :index
+    logged_in_as :instructor do
+      context "with json format" do
+        setup do
+          get :index, { }, :format => :json
+        end
+        should respond_with :success
+        should respond_with_content_type(/json/)
+        should_have_json_element('location')
+      end
+    end
   end
+  # def test_index
+  #   get :index
+  #   assert_template 'index'
+  # end
 
-  def test_create_invalid
-    LocationSuggestion.any_instance.stubs(:valid?).returns(false)
-    post :create
-    assert_template 'new'
-  end
+  # def test_show
+  #   get :show, :id => LocationSuggestion.first
+  #   assert_template 'show'
+  # end
 
-  def test_create_valid
-    LocationSuggestion.any_instance.stubs(:valid?).returns(true)
-    post :create
-    assert_redirected_to location_suggestion_url(assigns(:location_suggestion))
-  end
+  # def test_new
+  #   get :new
+  #   assert_template 'new'
+  # end
 
-  def test_edit
-    get :edit, :id => LocationSuggestion.first
-    assert_template 'edit'
-  end
+  # def test_create_invalid
+  #   LocationSuggestion.any_instance.stubs(:valid?).returns(false)
+  #   post :create
+  #   assert_template 'new'
+  # end
 
-  def test_update_invalid
-    LocationSuggestion.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => LocationSuggestion.first
-    assert_template 'edit'
-  end
+  # def test_create_valid
+  #   LocationSuggestion.any_instance.stubs(:valid?).returns(true)
+  #   post :create
+  #   assert_redirected_to location_suggestion_url(assigns(:location_suggestion))
+  # end
 
-  def test_update_valid
-    LocationSuggestion.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => LocationSuggestion.first
-    assert_redirected_to location_suggestion_url(assigns(:location_suggestion))
-  end
+  # def test_edit
+  #   get :edit, :id => LocationSuggestion.first
+  #   assert_template 'edit'
+  # end
 
-  def test_destroy
-    location_suggestion = LocationSuggestion.first
-    delete :destroy, :id => location_suggestion
-    assert_redirected_to location_suggestions_url
-    assert !LocationSuggestion.exists?(location_suggestion.id)
-  end
+  # def test_update_invalid
+  #   LocationSuggestion.any_instance.stubs(:valid?).returns(false)
+  #   put :update, :id => LocationSuggestion.first
+  #   assert_template 'edit'
+  # end
+
+  # def test_update_valid
+  #   LocationSuggestion.any_instance.stubs(:valid?).returns(true)
+  #   put :update, :id => LocationSuggestion.first
+  #   assert_redirected_to location_suggestion_url(assigns(:location_suggestion))
+  # end
+
+  # def test_destroy
+  #   location_suggestion = LocationSuggestion.first
+  #   delete :destroy, :id => location_suggestion
+  #   assert_redirected_to location_suggestions_url
+  #   assert !LocationSuggestion.exists?(location_suggestion.id)
+  # end
 end
