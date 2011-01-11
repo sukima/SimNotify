28f99504ae428b6468245f0129e0c6de1af2c2e0
Devin Weaver <suki@tritarget.org>
2011-01-11 16:29:15 -0500

    Refactored location_suggestions_test
    Changes flash notice in controller

--- test/functional/location_suggestions_controller_test.rb
+++ test/functional/location_suggestions_controller_test.rb
@@ -5,67 +5,80 @@ class LocationSuggestionsControllerTest < ActionController::TestCase
 
   should_map_resources :location_suggestions
 
-  context "get :index" do
-    should_require_logged_in :action => :index
-    logged_in_as :instructor do
-      context "with json format" do
-        setup do
-          get :index, { }, :format => :json
-        end
-        should respond_with :success
-        should respond_with_content_type(/json/)
-        should_have_json_element('location')
+  should_require_logged_in :action => :index
+  should_require_admin :action => :index
+  should_require_admin :action => :new
+  should_require_admin :action => :edit
+  should_require_admin :action => :create, :method => :post
+  should_require_admin :action => :update, :method => :post, :params => { :id => 1 }
+  should_require_admin :action => :destroy, :params => { :id => 1 }
+
+  logged_in_as :instructor do
+    context "get :index with json format" do
+      setup do
+        get :index, :format => "json"
+      end
+      should respond_with :success
+      should respond_with_content_type(/json/)
+      should "return a json array" do
+        assert_match /\[.*\]/, @response.body
       end
     end
   end
-  # def test_index
-  #   get :index
-  #   assert_template 'index'
-  # end
 
-  # def test_show
-  #   get :show, :id => LocationSuggestion.first
-  #   assert_template 'show'
-  # end
+  logged_in_as :admin do
+    context "get :index" do
+      setup do
+        get :index
+      end
+      should respond_with :success
+      should render_template 'index'
+    end
 
-  # def test_new
-  #   get :new
-  #   assert_template 'new'
-  # end
+    context "GET :new" do
+      setup do
+        get :new
+      end
+      should_respond_with :success
+      should_render_template :new
+    end
 
-  # def test_create_invalid
-  #   LocationSuggestion.any_instance.stubs(:valid?).returns(false)
-  #   post :create
-  #   assert_template 'new'
-  # end
+    context "POST :create" do
+      setup do
+        @old_count = LocationSuggestions.count
+        post :create, :location_suggestion => { :location => "foobar" }
+      end
+      should "increase count by 1" do
+        assert LocationSuggestions.count - @old_count == 1
+      end
+      should_redirect_to(":show") { location_suggestion_path(LocationSuggestions.last) }
+    end
+    
+    context "GET :show" do
+      setup do
+        # id only used to route properly
+        get :show, :id => 1
+      end
+      # no ID needed as this is just a redirect
+      should redirect_to(":index") { location_suggestions_path }
+    end
 
-  # def test_create_valid
-  #   LocationSuggestion.any_instance.stubs(:valid?).returns(true)
-  #   post :create
-  #   assert_redirected_to location_suggestion_url(assigns(:location_suggestion))
-  # end
+    context "PUT :update" do
+      setup do
+        put :update, :id => 1, :location_suggestion => { :location => "barfoo" }
+      end
+      should_redirect_to(":show") { location_suggestion_path(LocationSuggestion.find(1)) }
+    end
 
-  # def test_edit
-  #   get :edit, :id => LocationSuggestion.first
-  #   assert_template 'edit'
-  # end
-
-  # def test_update_invalid
-  #   LocationSuggestion.any_instance.stubs(:valid?).returns(false)
-  #   put :update, :id => LocationSuggestion.first
-  #   assert_template 'edit'
-  # end
-
-  # def test_update_valid
-  #   LocationSuggestion.any_instance.stubs(:valid?).returns(true)
-  #   put :update, :id => LocationSuggestion.first
-  #   assert_redirected_to location_suggestion_url(assigns(:location_suggestion))
-  # end
-
-  # def test_destroy
-  #   location_suggestion = LocationSuggestion.first
-  #   delete :destroy, :id => location_suggestion
-  #   assert_redirected_to location_suggestions_url
-  #   assert !LocationSuggestion.exists?(location_suggestion.id)
-  # end
+    context "GET :destroy" do
+      setup do
+        @old_count = LocationSuggestion.count
+        delete :destroy, :id => 1
+      end
+      should "decrease count by 1" do
+        assert LocationSuggestion.count - @old_count == -1
+      end
+      should_redirect_to(":index") { location_suggestions_path }
+    end
+  end # logged_in_as :admin
 end
