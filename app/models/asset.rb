6104f67728f3205a0170532d0f59fb6fdf20249d
Devin Weaver <suki@tritarget.org>
2011-02-15 17:56:02 -0500

    Adds validation checks to asset model

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,11 +3,14 @@ class Asset < ActiveRecord::Base
   belongs_to :instructor
   has_and_belongs_to_many :events
 
+  validates_attachment_presence :session_asset
+  validates_attachment_size :session_asset, :less_then => 20.megabytes
+
   # Prevent processing for non-image files.
   # Referenced from blog post:
   # http://awesomeful.net/posts/33-attach-non-image-files-in-rails-with-paperclip
   before_post_process :image?
   def image?
-    !(data_content_type =~ /^image.*/).nil?
+    !(session_asset_content_type =~ /^image.*/).nil?
   end
 end
--- test/unit/asset_test.rb
+++ test/unit/asset_test.rb
@@ -3,4 +3,7 @@ require 'test_helper'
 class AssetTest < ActiveSupport::TestCase
   should belong_to :instructor
   should have_and_belong_to_many :events
+  should_have_attached_file :session_asset
+  should_validate_attachment_presence :session_asset
+  should_validate_attachment_size :session_asset, :less_then => 20.megabytes
 end
