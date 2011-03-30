13d7c7fa33769a5bcfa1a5fd90f0af3f44a69600
Devin Weaver <suki@tritarget.org>
2011-03-30 18:17:52 -0400

    Adds active record sessions store instead of cookies
    Passenger fails with cookies. Since we can't upgrade passenger we work around
    it with active records store instead.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110324132850) do
+ActiveRecord::Schema.define(:version => 20110330221630) do
 
   create_table "assets", :force => true do |t|
     t.string   "session_asset_file_name"
@@ -121,6 +121,16 @@ ActiveRecord::Schema.define(:version => 20110324132850) do
     t.integer "manikin_req_type_id"
     t.text    "title",               :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "special_events", :force => true do |t|
     t.string   "title"
