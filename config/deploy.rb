7648177a1cc65f61332006ecb38beb4dcb4e5a6b
Devin Weaver <suki@tritarget.org>
2011-03-30 15:32:49 -0400

    Adds Capistrano rules for attachments in shared_folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,7 @@ set :domain, 'delphinus.dreamhost.com'  # Dreamhost servername where your accoun
 set :project, 'SimNotify'  # Your application as its called in the repository
 set :application, 'schedulesimulation.com'  # Your app's location (domain or sub-domain name as setup in panel)
 set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
+set :shared_assets, "public/images/attachments"
 
 # Don't change this stuff, but you may want to set shared files at the end of the file ##################
 # deploy config
@@ -33,13 +34,36 @@ namespace :deploy do
  end
 end
 
+namespace :assets  do
+  namespace :symlinks do
+    desc "Setup application symlinks for shared assets"
+    task :setup, :roles => [:app, :web] do
+      shared_assets.each { |link| run "mkdir -p #{shared_path}/#{link}" }
+    end
+
+    desc "Link assets for current deploy to the shared location"
+    task :update, :roles => [:app, :web] do
+      shared_assets.each { |link| run "ln -nfs #{shared_path}/#{link} #{release_path}/#{link}" }
+    end
+  end
+end
+
 # additional settings
 default_run_options[:pty] = true  # Forgo errors when deploying from windows
 #ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
 #set :chmod755, "app config db lib public vendor script script/* public/disp*"
 set :use_sudo, false
 
+
 #########################################################################################################
+
+before "deploy:setup" do
+  assets.symlinks.setup
+end
+
+before "deploy:symlink" do
+  assets.symlinks.update
+end
 
 #for use with shared files (e.g. config files)
 after "deploy:update_code" do
