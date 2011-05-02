d1cb9418184d0df6fa40f17a308648cfd0c3a9ee
Devin Weaver <suki@tritarget.org>
2011-05-02 17:29:59 -0400

    Fixes hint bug in formtastic custom inputs
    When displaying a custm input (ie. autocomplete or multiselect) the
    hints would duplicate and print out twice. This completely refractors the
    formtastic custom library to eliminate the problem.

--- lib/application_formtastic_builder.rb
+++ lib/application_formtastic_builder.rb
@@ -1,15 +1,17 @@
 class ApplicationFormtasticBuilder < Formtastic::SemanticFormBuilder
   def autocomplete_input(method, options = {})
-    input_html = { :class => 'autocomplete' }
-    input_html = options[:input_html].merge(input_html) unless options[:input_html].nil?
-    options = options.merge({ :input_html => input_html })
-    input method, options
+    input_html = options[:input_html] || {}
+    html_class = [ 'autocomplete' ]
+    input_html[:class] = (html_class << input_html[:class]).flatten.compact.join(' ')
+    options[:input_html] = input_html
+    text_input method, options
   end
 
   def multiselect_input(method, options = {})
-    input_html = { :class => 'multiselect' }
-    input_html = options[:input_html].merge(input_html) unless options[:input_html].nil?
-    options = options.merge({ :input_html => input_html })
-    input method, options
+    input_html = options[:input_html] || {}
+    html_class = [ 'multiselect' ]
+    input_html[:class] = (html_class << input_html[:class]).flatten.compact.join(' ')
+    options[:input_html] = input_html
+    select_input method, options
   end
 end
