eee9a5c77e42563fa592425a8e5c193b990741ed
Devin Weaver <suki@tritarget.org>
2011-05-03 08:17:40 -0400

    Adds multiple option to multiselect input

--- lib/application_formtastic_builder.rb
+++ lib/application_formtastic_builder.rb
@@ -1,15 +1,16 @@
 class ApplicationFormtasticBuilder < Formtastic::SemanticFormBuilder
   def autocomplete_input(method, options = {})
-    input_html = options[:input_html] || {}
     html_class = [ 'autocomplete' ]
+    input_html = options[:input_html] || {}
     input_html[:class] = (html_class << input_html[:class]).flatten.compact.join(' ')
     options[:input_html] = input_html
     text_input method, options
   end
 
   def multiselect_input(method, options = {})
-    input_html = options[:input_html] || {}
     html_class = [ 'multiselect' ]
+    input_html = options[:input_html] || {}
+    input_html[:multiple] = true
     input_html[:class] = (html_class << input_html[:class]).flatten.compact.join(' ')
     options[:input_html] = input_html
     select_input method, options
