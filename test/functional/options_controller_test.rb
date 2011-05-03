1ec28da9626aeb098edce414e606af3440490760
Devin Weaver <suki@tritarget.org>
2011-05-03 06:59:30 -0400

    Fixes several testing errors

--- test/functional/options_controller_test.rb
+++ test/functional/options_controller_test.rb
@@ -2,27 +2,33 @@ require 'test_helper'
 
 class OptionsControllerTest < ActionController::TestCase
   setup :activate_authlogic
+  except_routes = [:show, :edit, :new, :create, :destroy]
 
-  should_map_resources :options, :only => [:index, :update]
+  should_map_resources :options, :except => except_routes
 
-  #should_require_logged_in :except
-  #should_require_admin :except
+  should_require_logged_in :except => except_routes
 
-  context "GET :index" do
-    setup do
-      get :index
-    end
-    should assign_to(:options)
-    should respond_with :success
-    should render_template :index
+  logged_in_as :instructor do
+    should_require_admin :except => except_routes
   end
 
-  context "PUT :update" do
-    setup do
-      @f = Factory(:option)
-      @f.value = "test_update_model_option_field_value"
-      put :update, :id => @f.id, :option => @f.attributes
+  logged_in_as :admin do
+    context "GET :index" do
+      setup do
+        get :index
+      end
+      should assign_to(:options)
+      should respond_with :success
+      should render_template :index
     end
-    should redirect_to(":index") { options_path }
+
+    context "PUT :update" do
+      setup do
+        @f = Factory(:option)
+        @f.value = "test_update_model_option_field_value"
+        put :update, :id => @f.id, :option => @f.attributes
+      end
+      should redirect_to(":index") { options_path }
+    end
   end
 end
