4ea3e0e2d669a9b918612991bf1848123f67087d
Devin Weaver <suki@tritarget.org>
2011-05-05 07:08:41 -0400

    Fixes multiple select option bug
    The select box would not highlight the values that were refelcted
    in the database. Nothing was selected. This fixes that issue.

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,12 @@
 class Option < ActiveRecord::Base
   serialize :value
+  before_update :sanatize_value
+
+  private
+  def sanatize_value
+    case name
+    when "system_email_recipients"
+      value.map! {|i| i.to_i}
+    end
+  end
 end
--- lib/application_formtastic_builder.rb
+++ lib/application_formtastic_builder.rb
@@ -10,7 +10,7 @@ class ApplicationFormtasticBuilder < Formtastic::SemanticFormBuilder
   def multiselect_input(method, options = {})
     html_class = [ 'multiselect' ]
     input_html = options[:input_html] || {}
-    input_html[:multiple] = true
+    options[:multiple] = true
     input_html[:class] = (html_class << input_html[:class]).flatten.compact.join(' ')
     options[:input_html] = input_html
     select_input method, options
