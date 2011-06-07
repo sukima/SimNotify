470d17a0aedd6743e19cc5e45b90200b2bcf6fb2
Devin Weaver <suki@tritarget.org>
2011-06-07 13:33:11 -0400

    Fixes several display issues
    Better agenda descriptions, better time display

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,2 +1,7 @@
 module CalendarHelper
+  def agenda_event_descrition_for(event)
+    str = "<em>#{event.title}</em> - #{session_description(event)}"
+    str << " - <strong>#{event.technician.name}</strong>" unless event.technician.nil?
+    return str
+  end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
   def session_description(event)
-    ret = h event.start_time
+    ret = dt event.start_time
     ret += " (#{distance_of_time_in_words(event.start_time, event.end_time)})"
     ret += " - #{pluralize(event.scenarios.count, "senario")}"
     ret += h event.need_flags_as_words if event.collective_has_needs
