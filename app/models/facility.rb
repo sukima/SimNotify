007ee2d0e7fd1ac37091feb38ea8e9f61f82cd46
Devin Weaver <suki@tritarget.org>
2011-06-11 17:20:58 -0400

    Normalizes agenda color in model for use in css

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,3 +1,7 @@
 class Facility < ActiveRecord::Base
   validates_presence_of :name
+
+  def agenda_color
+    return (self[:agenda_color] =~ /^#/) ? self[:agenda_color] : "##{self[:agenda_color]}"
+  end
 end
