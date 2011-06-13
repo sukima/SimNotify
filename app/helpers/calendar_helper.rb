ea5941dcde06368140f594b3813fa89cae9085cd
Devin Weaver <suki@tritarget.org>
2011-06-13 07:53:29 -0400

    Fixes helper methods to work with both event types

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,7 +1,11 @@
 module CalendarHelper
   def agenda_event_descrition_for(event)
     str = "<em>#{event.title}</em> - #{session_description(event)}"
-    str << " - <strong>#{event.technician.name}</strong>" unless event.technician.nil?
+    if event.kind_of? Event
+      if !event.technician.nil?
+        str << " - <strong>#{event.technician.name}</strong>"
+      end
+    end
     return str
   end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,8 +2,10 @@ module EventsHelper
   def session_description(event)
     ret = dt event.start_time
     ret += " (#{distance_of_time_in_words(event.start_time, event.end_time)})"
-    ret += " - #{pluralize(event.scenarios.count, "senario")}"
-    ret += h event.need_flags_as_words if event.collective_has_needs
+    if event.kind_of? Event
+      ret += " - #{pluralize(event.scenarios.count, "senario")}"
+      ret += h event.need_flags_as_words if event.collective_has_needs
+    end
     return ret
   end
 
