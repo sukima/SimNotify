baa7ce3fa939dc43edee99fa36fc3d3d4b3cbf92
Devin Weaver <suki@tritarget.org>
2011-06-13 07:55:40 -0400

    Revamps calendar code to pass with new tests
    Fixes better background color in facilities. Changes business
    logic in calendar controller.

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -2,7 +2,7 @@ class Facility < ActiveRecord::Base
   validates_presence_of :name
 
   def agenda_color
-    return "#369" if self[:agenda_color].blank?
+    return "#36c" if self[:agenda_color].blank?
     return (self[:agenda_color] =~ /^#/) ? self[:agenda_color] : "##{self[:agenda_color]}"
   end
 end
