21b995aaa563d5b6e5234116ac21fdbda46c21c1
Devin Weaver <suki@tritarget.org>
2011-07-08 14:40:36 -0400

    Adds inactive_notification_for helper

--- app/helpers/instructors_helper.rb
+++ app/helpers/instructors_helper.rb
@@ -6,4 +6,11 @@ module InstructorsHelper
     return "" if roles.empty?
     "<p><strong>Roles:</strong> #{h roles.to_sentence}</p>"
   end
+
+  def inactive_notification_for(instructor)
+    inactive_style = (instructor.active?) ? "display:none" : ""
+    content_tag :div, {:id => "instructor_inactive_warning", :class => "warning", :style => inactive_style } do
+      "This instructor is locked! He or She <strong>can not</strong> log into #{APP_CONFIG[:application_name]}."
+    end
+  end
 end
