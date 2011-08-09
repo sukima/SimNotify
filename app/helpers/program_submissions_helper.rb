a0d096d55b6ead98edf6f740758540016043b8aa
Devin Weaver <suki@tritarget.org>
2011-08-09 18:05:32 -0400

    Fixes several bugs in program_submission
    Fixes Bad confirm on destroy link.
    Adds a link_to_program_submission helper so index view has links to the correct show views.
    Adds delete to routing.

--- app/helpers/program_submissions_helper.rb
+++ app/helpers/program_submissions_helper.rb
@@ -1,2 +1,5 @@
 module ProgramSubmissionsHelper
+  def link_to_program_submission(title, program_submission)
+    return link_to title, program_submission_path(program_submission)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :options, :only=> [:index, :update]
 
   map.resources :programs
-  map.resources :program_submissions, :except => [ :edit, :update ]
+  map.resources :program_submissions, :except => [ :edit, :update ], :member => { :delete => :get }
 
   # The priority is based upon order of creation: first created -> highest priority.
 
