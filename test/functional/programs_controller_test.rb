766bd2a9178c8a1585f08ad98a9002a178a23a17
Devin Weaver <suki@tritarget.org>
2011-08-11 22:14:17 -0400

    Innitial changes to logged_in_as

--- test/functional/programs_controller_test.rb
+++ test/functional/programs_controller_test.rb
@@ -1,8 +1,54 @@
 require 'test_helper'
 
 class ProgramsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup :activate_authlogic
+
+  should_map_resources :programs
+
+  logged_in_as :instructor do
+    context "GET :index" do
+      setup do
+        get :index
+      end
+      should assign_to(:programs)
+      should respond_with :success
+      should render_template :index
+    end
+
+    context "GET :show" do
+      setup do
+        @f = Factory(:program)
+        get :show, :id => @f.id
+      end
+      should assign_to(:program)
+      should respond_with :success
+      should render_template :show
+    end
+
+    context "GET :new" do
+      setup do
+        get :new
+      end
+      should_require_adminX :redirect => :programs_path
+    end
+
+    # context "PUT :update" do
+      # setup do
+        # @f = Factory(:program)
+        # @f.benifit = "test_update_model_program_field_benifit"
+        # put :update, :id => @f.id, :program => @f.attributes
+      # end
+      # should_require_admin
+    # end
+
+    # context "GET :edit" do
+      # setup do
+        # @f = Factory(:program)
+        # get :edit, :id => @f.id
+      # end
+      # should_require_admin
+    # end
+
+    # #testdestroy
   end
 end
--- test/shoulda_macros/logged_in_as.rb
+++ test/shoulda_macros/logged_in_as.rb
@@ -10,4 +10,31 @@ class Test::Unit::TestCase
       yield
     end
   end
+
+  def self.should_require_logged_inX(opt={})
+    opt[:flash] ||= I18n.translate(:login_required)
+
+    should redirect_to(":login") { login_path }
+
+    if opt[:flash]
+      should set_the_flash.to(opt[:flash])
+    end
+  end
+
+  def self.should_require_adminX(opt={})
+    opt[:flash] ||= I18n.translate(:admin_required)
+    opt[:redirect] ||= false
+
+    if opt[:redirect]
+      if opt[:redirect].kind_of? String
+        should redirect_to(opt[:redirect])
+      else
+        should redirect_to(opt[:redirect].to_s) { send(opt[:redirect]) }
+      end
+    end
+
+    if opt[:flash]
+      should set_the_flash.to(opt[:flash])
+    end
+  end
 end
