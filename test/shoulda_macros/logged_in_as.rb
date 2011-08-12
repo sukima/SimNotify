86af293b31e51d4c3c19e6d804b13815bb910c14
Devin Weaver <suki@tritarget.org>
2011-08-12 15:25:28 -0400

    Adds REST action checks for logged_in_as
    Includes VIM folding and comment docs

--- test/shoulda_macros/logged_in_as.rb
+++ test/shoulda_macros/logged_in_as.rb
@@ -25,7 +25,7 @@ class Test::Unit::TestCase # :nodoc:
   #       end
   #     end
   #   end
-  def self.logged_in_as(person, &block)
+  def self.logged_in_as(person, &block) # {{{
     context "logged in as #{person.to_s}" do
       setup do
         @instructor = Factory(person)
@@ -34,8 +34,7 @@ class Test::Unit::TestCase # :nodoc:
 
       yield
     end
-  end
-
+  end # }}}
 
   # Checks the redirect in the case of not being logged in correctly.
   #
@@ -52,7 +51,7 @@ class Test::Unit::TestCase # :nodoc:
   #   should_check_logged_in_redirect( :root_path )
   #   should_check_logged_in_redirect( lambda { root_path } )
   #
-  def self.should_check_logged_in_redirect(redirect)
+  def self.should_check_logged_in_redirect(redirect) # {{{
     # There is a bug in the shoulda version 2.11.3 that will not read the
     # description string sent to redirect_to. Upgrading to shoulda-matchers
     # and shoulda-contexts beta makes things worse because the set_flash_to
@@ -75,8 +74,7 @@ class Test::Unit::TestCase # :nodoc:
         end
       end
     end
-  end
-
+  end # }}}
 
   # Checks that the flash was set in the case of not being logged in correctly.
   #
@@ -92,14 +90,13 @@ class Test::Unit::TestCase # :nodoc:
   #   should_check_logged_in_flash("test denied")
   #   should_check_logged_in_flash(/test/)
   #
-  def self.should_check_logged_in_flash(expected_flash=nil)
+  def self.should_check_logged_in_flash(expected_flash=nil) # {{{
     if expected_flash.kind_of? String or expected_flash.kind_of? Regexp
       should set_the_flash.to(expected_flash)
     else
       should set_the_flash
     end
-  end
-
+  end # }}}
 
   # Checks that the session return_to was set in the case of not being logged
   # in correctly.
@@ -116,7 +113,7 @@ class Test::Unit::TestCase # :nodoc:
   #   should_check_logged_in_return_to( :root_path )
   #   should_check_logged_in_return_to( lambda { root_path } )
   #
-  def self.should_check_logged_in_return_to(return_to)
+  def self.should_check_logged_in_return_to(return_to) # {{{
     if return_to.kind_of? String
       should "set the :return_to session variable to #{return_to}" do
         assert_equal return_to, session[:return_to]
@@ -133,8 +130,7 @@ class Test::Unit::TestCase # :nodoc:
         end
       end
     end
-  end
-
+  end # }}}
 
   # Checks the basic tests that are needed when a user is not logged in
   # correctly.
@@ -146,12 +142,11 @@ class Test::Unit::TestCase # :nodoc:
   #     :redirect => See should_check_logged_in_redirect()
   #     :return_to => See should_check_logged_in_return_to()
   #
-  def self.should_check_logged_in_status(opt)
+  def self.should_check_logged_in_status(opt) # {{{
     self.should_check_logged_in_flash(opt[:flash]) if opt[:flash]
     self.should_check_logged_in_redirect(opt[:redirect]) if opt[:redirect]
     self.should_check_logged_in_return_to(opt[:return_to]) if opt[:return_to]
-  end
-
+  end # }}}
 
   # Convinience method to test if controller properly handled a user who is not
   # logged in.
@@ -170,13 +165,12 @@ class Test::Unit::TestCase # :nodoc:
   #   should_require_logged_in_access :flash => /test/, :return_to => "controller/index"
   #   should_require_logged_in_access :redirect => lambda { root_path }
   #
-  def self.should_require_logged_in_access(opt={})
+  def self.should_require_logged_in_access(opt={}) # {{{
     opt[:flash] ||= I18n.translate(:login_required)
     opt[:redirect] ||= :login_path
     opt[:return_to] ||= false
     self.should_check_logged_in_status(opt)
-  end
-
+  end # }}}
 
   # Convinience method to test if controller properly handled a user who is not
   # an admin.
@@ -195,10 +189,177 @@ class Test::Unit::TestCase # :nodoc:
   #   should_require_admin_access :flash => /test/, :return_to => "controller/index"
   #   should_require_admin_access :redirect => lambda { root_path }
   #
-  def self.should_require_admin_access(opt={})
+  def self.should_require_admin_access(opt={}) # {{{
     opt[:flash] ||= I18n.translate(:admin_required)
     opt[:redirect] ||= :login_path
     opt[:return_to] ||= false
     self.should_check_logged_in_status(opt)
-  end
+  end # }}}
+
+  # Checks REST actions for correct login or admin access.
+  # Ment to be called from should_require_admin_access_for_resources()
+  # and should_require_logged_in_access_for_resources()
+  #
+  # Options:
+  #
+  #   :admin => boolean
+  #   :only => Array of actions, or one symbol
+  #   :except => Array of actions, or one symbol
+  #   See should_check_logged_in_status() for more options
+  #
+  # Defaults:
+  #
+  #   :admin => false
+  #   All actions [ :index, :new, :create, :show, :edit, :update, :destroy ]
+  #   See should_require_logged_in_access() and should_require_admin_access()
+  #   for more defaults
+  #
+  def self.should_require_access_for_resources(opt) # {{{
+    opt[:admin] ||= false
+    actions = self.build_actions_list(opt)
+
+    if actions.include? :index
+      context "GET :index" do
+        setup do
+          get :index
+        end
+        if opt[:admin] then should_require_admin_access(opt)
+        else should_require_logged_in_access(opt) end
+      end
+    end
+
+    if actions.include? :new
+      context "GET :new" do
+        setup do
+          get :new
+        end
+        if opt[:admin] then should_require_admin_access(opt)
+        else should_require_logged_in_access(opt) end
+      end
+    end
+
+    if actions.include? :create
+      context "POST :create" do
+        setup do
+          post :create
+        end
+        if opt[:admin] then should_require_admin_access(opt)
+        else should_require_logged_in_access(opt) end
+      end
+    end
+
+    if actions.include? :show
+      context "GET :show" do
+        setup do
+          get :show, :id => 1
+        end
+        if opt[:admin] then should_require_admin_access(opt)
+        else should_require_logged_in_access(opt) end
+      end
+    end
+
+    if actions.include? :edit
+      context "GET :edit" do
+        setup do
+          get :edit, :id => 1
+        end
+        if opt[:admin] then should_require_admin_access(opt)
+        else should_require_logged_in_access(opt) end
+      end
+    end
+
+    if actions.include? :update
+      context "PUT :update" do
+        setup do
+          put :update, :id => 1
+        end
+        if opt[:admin] then should_require_admin_access(opt)
+        else should_require_logged_in_access(opt) end
+      end
+    end
+
+    if actions.include? :destroy
+      context "GET :destroy" do
+        setup do
+          delete :destroy, :id => 1
+        end
+        if opt[:admin] then should_require_admin_access(opt)
+        else should_require_logged_in_access(opt) end
+      end
+    end
+  end # }}}
+
+  # Checks REST actions for correct login access.
+  #
+  # Options:
+  #
+  #   :only => Array of actions, or one symbol
+  #   :except => Array of actions, or one symbol
+  #   See should_check_logged_in_status() for more options
+  #
+  # Examples:
+  #
+  #   should_require_logged_in_access_for_resources :except => :destroy
+  #   should_require_logged_in_access_for_resources :only => [ :index, :show ]
+  #
+  def self.should_require_logged_in_access_for_resources(opt={}) # {{{
+    opt[:admin] = false
+    self.should_require_access_for_resources(opt)
+  end # }}}
+
+  # Checks REST actions for correct admin access.
+  #
+  # Options:
+  #
+  #   :only => Array of actions, or one symbol
+  #   :except => Array of actions, or one symbol
+  #   See should_check_logged_in_status() for more options
+  #
+  # Examples:
+  #
+  #   should_require_admin_access_for_resources :except => :destroy
+  #   should_require_admin_access_for_resources :only => [ :index, :show ]
+  #
+  def self.should_require_admin_access_for_resources(opt={}) # {{{
+    opt[:admin] = true
+    self.should_require_access_for_resources(opt)
+  end # }}}
+
+
+  private
+
+  # Builds a list of REST actions.
+  # By deafult will return the entire list of REST actions.
+  #
+  # Options:
+  #
+  #   :only => Array of actions, or one symbol
+  #   :except => Array of actions, or one symbol
+  #
+  # Defaults:
+  #
+  #   All actions [ :index, :new, :create, :show, :edit, :update, :destroy ]
+  #
+  def self.build_actions_list(opt) # {{{
+    opt[:except] ||= nil
+    opt[:only] ||= nil
+
+    actions = [ :index, :new, :create, :show, :edit, :update, :destroy ]
+
+    if opt[:only]
+      actions = [ ]
+      Array(opt[:only]).each do |o|
+        actions.push(o)
+      end
+    end
+
+    if opt[:except]
+      opt[:except].each do |e|
+        actions.delete(e)
+      end
+    end
+
+    return actions
+  end # }}}
 end
+# vim:set sw=2 ts=2 sts=2 et fdm=marker:
