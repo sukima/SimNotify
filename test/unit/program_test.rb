c6e7d2d99ad1a7414f7ee0d1ff94a66022ae3e4b
Devin Weaver <suki@tritarget.org>
2011-08-11 22:11:07 -0400

    Fixes bad program model
    Adds tests. corrected belongs_to. Adds uniqueness of :name

--- test/unit/program_test.rb
+++ test/unit/program_test.rb
@@ -2,4 +2,11 @@ require 'test_helper'
 
 class ProgramTest < ActiveSupport::TestCase
   should belong_to :contact
+  should belong_to :created_by
+
+  should validate_presence_of :name
+  should validate_presence_of :description
+  should validate_presence_of :benifit
+
+  should validate_uniquness_of :name
 end
