cd20664c2391cd99ed2bcbb81a3c2df885532e3b
Devin Weaver <suki@tritarget.org>
2011-08-11 22:13:21 -0400

    Adds actions to programs_controller

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,2 +1,31 @@
 class ProgramsController < ApplicationController
+  before_filter :login_required
+  before_filter :login_admin, :except => [ :index, :show ]
+
+  def index
+    @programs = Program.all
+
+    respond_to do |wants|
+      wants.html # index.html.erb
+      wants.xml  { render :xml => @programs }
+    end
+  end
+
+  def show
+    @program = Program.find(params[:id])
+
+    respond_to do |wants|
+      wants.html # show.html.erb
+      wants.xml  { render :xml => @program }
+    end
+  end
+
+  def new
+    @program = Program.new
+
+    respond_to do |wants|
+      wants.html # new.html.erb
+      wants.xml  { render :xml => @program }
+    end
+  end
 end
