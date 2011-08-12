c8330646eb5263ba75fd931c3a90232470819a39
Devin Weaver <suki@tritarget.org>
2011-08-12 15:24:42 -0400

    Adds more actions to programs_controller
    Adds more view files

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -28,4 +28,48 @@ class ProgramsController < ApplicationController
       wants.xml  { render :xml => @program }
     end
   end
+
+  def create
+    @program = Program.new(params[:program])
+
+    respond_to do |wants|
+      if @program.save
+        flash[:notice] = 'Program was successfully created.'
+        wants.html { redirect_to(@program) }
+        wants.xml  { render :xml => @program, :status => :created, :location => @program }
+      else
+        wants.html { render :action => "new" }
+        wants.xml  { render :xml => @program.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @program = Program.find(params[:id])
+  end
+
+  def update
+    @program = Program.find(params[:id])
+
+    respond_to do |wants|
+      if @program.update_attributes(params[:program])
+        flash[:notice] = 'Program was successfully updated.'
+        wants.html { redirect_to(@program) }
+        wants.xml  { head :ok }
+      else
+        wants.html { render :action => "edit" }
+        wants.xml  { render :xml => @program.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @program = Program.find(params[:id])
+    @program.destroy
+
+    respond_to do |wants|
+      wants.html { redirect_to(programs_url) }
+      wants.xml  { head :ok }
+    end
+  end
 end
