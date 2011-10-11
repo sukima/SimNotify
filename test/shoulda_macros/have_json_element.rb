c9d372093da7cbc5e4a72cb49d1e2be75d968281
Devin Weaver <suki@tritarget.org>
2011-10-11 17:08:49 -0400

    Adds should_have_json_elements test macro

--- test/shoulda_macros/have_json_element.rb
+++ test/shoulda_macros/have_json_element.rb
@@ -1,9 +1,52 @@
 class Test::Unit::TestCase
   def self.should_have_json_element(element)
-    should "have #{element}" do
+    # DEPRECATED. Use should_have_json_elements
+    should "have #{element} element" do
       assert_not_nil @response
       assert @response.respond_to? :body
       assert_match /"#{element}":/, @response.body
+    end
+  end
+
+  def self.construct_json_object(json_obj)
+    if json_obj.kind_of? String
+      _json = ActiveSupport::JSON.decode(json_obj)
+    elsif json_obj.kind_of? ActiveSupport::JSON
+      _json = json_obj
+    else
+      _json = nil
+    end
+    _json
+  end
+
+  def self.should_have_json_elements(elements, json_obj, should_not=false)
+    context "JSON object" do
+      _json = construct_json_object(json_obj)
+      elements.each do |e|
+        if should_not
+          should "not have #{e} element" do
+            assert !_json.respond_to?(e.to_sym), "JSON object does respond_to? #{e}"
+          end
+        else
+          should "have #{e} element" do
+            assert _json.respond_to?(e.to_sym), "JSON object does not respond_to? #{e}"
+          end
+        end
+      end
+    end
+  end
+
+  def self.should_not_have_json_elements(elements, json_obj)
+    should_not_have_json_elements(elements, json_obj, true)
+  end
+
+  def self.should_have_json_array(json_obj)
+    context "JSON object" do
+      _json = construct_json_object(json_obj)
+      should "be a JSON array" do
+        assert_not_nil _json, "JSON object is nil"
+        assert _json.kind_of? Array, "JSON object is not an Array"
+      end
     end
   end
 end
